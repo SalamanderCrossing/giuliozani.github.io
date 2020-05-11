01e0c809a55d7867f210f6e1c1ba7d55217600c4
GiulioZani <giulio.zani@gmail.com>
2020-05-12 01:04:57 +0200

    fixes

--- focus/js/main.js
+++ focus/js/main.js
@@ -30,41 +30,40 @@ const data = {
 };
 
 $(document).ready(() => {
-  setTimeout(()=>{
-  const canvas = document.getElementById("plotting_canvas");
-    canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  canvas.style.position = "fixed";
-
-  $(".Calibration").hide();
-  initWebgazer(()=>{
-
-  const walker = new Walker("plotting_canvas");
-  walker.start();
-
-  const minutes = 1.0;
-
   setTimeout(() => {
-    walker.stop();
-    webgazer.end();
-    const rx = corr(data.walker.xs, data.eye.xs);
-    const ry = corr(data.walker.ys, data.eye.ys);
-    const r = (rx + ry) / 2;
+    const canvas = document.getElementById("plotting_canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.position = "fixed";
 
-    Swal.fire(`Accuracy=${r}`, "good job", "success");
-  }, minutes * 1000 * 60);
+    $(".Calibration").hide();
+    initWebgazer(() => {
+      webgazer.showPredictionPoints(false);
+      const walker = new Walker("plotting_canvas");
+      walker.start();
 
-  webgazer.setGazeListener((eyePosition, clock) => {
-    //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-    //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
-    if (eyePosition){
-      data.eye.xs.push(eyePosition.x);
-      data.eye.ys.push(eyePosition.y);
-      data.walker.xs.push(walker.x);
-      data.walker.ys.push(walker.y);
-    }
-  });
-  });
+      const minutes = 1.0;
 
-  }, 1000)
+      setTimeout(() => {
+        walker.stop();
+        webgazer.end();
+        const rx = corr(data.walker.xs, data.eye.xs);
+        const ry = corr(data.walker.ys, data.eye.ys);
+        const r = (rx + ry) / 2;
+
+        Swal.fire(`Accuracy=${r}`, "good job", "success");
+      }, minutes * 1000 * 60);
+
+      webgazer.setGazeListener((eyePosition, clock) => {
+        //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+        //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+        if (eyePosition) {
+          data.eye.xs.push(eyePosition.x);
+          data.eye.ys.push(eyePosition.y);
+          data.walker.xs.push(walker.x);
+          data.walker.ys.push(walker.y);
+        }
+      });
+    });
+  }, 1000);
 });
