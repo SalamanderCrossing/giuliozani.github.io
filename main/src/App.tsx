6eaeb821b2065b16b42a79c91988f9086bf3a16f
salamandercrossing <giulio.zani@gmail.com>
2022-12-19 16:24:25 +0000

    update

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -12,6 +12,7 @@ const FocusGraph = (
   const fgRef = useRef();
   const [selectedNode, setSelectedNode] = useState<string | null>(null);
   const [hide, setHide] = useState(false);
+
   const handleClick = useCallback(
     (node: { id: string; x: number; y: number; z: number }) => {
       // Aim at node from outside it
@@ -27,7 +28,7 @@ const FocusGraph = (
       fgRef!.current!.cameraPosition(
         { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
         // now look at the node but a bit shifted up
-        { x: node.x, y: node.y - 20, z: node.z },
+        { x: node.x, y: node.y, z: node.z },
         animationDuration, // ms transition duration
       );
     },
@@ -36,7 +37,7 @@ const FocusGraph = (
   // css to make the display appear on top of the graph
   const cssToAddToDisplay = {
     position: "absolute",
-    top: "25%",
+    top: "10%",
     left: "50%",
     transform: "translate(-50%, 0)",
     zIndex: 1,
@@ -47,11 +48,21 @@ const FocusGraph = (
   return (
     <div>
       <ForceGraph3D
-        style={{ position: "relative", top: 0, left: 0, right: 0, bottom: 0 }}
+        style={{
+          position: "relative",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
         ref={fgRef}
+        // makes the selected node much more transparent
+        nodeColor={(node) =>
+          node.id === selectedNode ? "rgba(255, 255, 255, 0.1)" : "#3a86c8"}
         //@ts-ignore
         graphData={data}
         nodeLabel="id"
+        linkDirectionalParticles={1}
         nodeAutoColorBy="id"
         //@ts-ignore
         onNodeClick={handleClick}
@@ -70,14 +81,14 @@ function App() {
   // those are Home, About, Contact, etc.
   const data = {
     nodes: [
-      { id: "Home", value: "Giulio Zani" },
+      { id: "Home" },
       { id: "About" },
-      { id: "Contact" },
+      { id: "Links & Contact" },
       { id: "Projects" },
     ],
     links: [
       { source: "Home", target: "About" },
-      { source: "Home", target: "Contact" },
+      { source: "Home", target: "Links & Contact" },
       { source: "Home", target: "Projects" },
     ],
   };
@@ -85,8 +96,8 @@ function App() {
   // of that view
   const displays = {
     Home: () => (
-      <div>
-        <h1>Giulio Zani</h1>
+      <div className="dark-bg">
+        <h1 className="title">GIULIO ZANI</h1>
         <img
           src="./profile.jpeg"
           alt="profile"
@@ -97,11 +108,74 @@ function App() {
             borderRadius: "50%",
           }}
         />
+        <p style={{ fontSize: "1.2em" }}>
+          As a dedicated artificial intelligence student and experienced private
+          tutor and consultant, you are a highly skilled and knowledgeable
+          professional in the field of computer programming, artificial
+          intelligence, and machine learning. With a passion for staying
+          up-to-date on the latest developments in these rapidly-evolving
+          fields, you are constantly seeking new opportunities to learn and
+          grow. Your expertise and enthusiasm for teaching make you an
+          invaluable asset to your clients and students, helping them to achieve
+          their goals and reach their full potential. Whether you are working
+          with students one-on-one or presenting at conferences and workshops,
+          you are always eager to share your knowledge and inspire others to
+          learn more about these exciting and transformative technologies.
+        </p>
       </div>
     ),
     About: () => <div>About</div>,
-    Contact: () => <div>Contact</div>,
-    Projects: () => <div>Projects</div>,
+    "Links & Contact": () => (
+      <div>
+        <h1>CONTACT</h1>
+        <h2>
+          g.zani(at)uva.nl
+        </h2>
+        <a
+          href="https://www.superprof.it/magistrale-intelligenza-artificiale-offre-lezioni-aiuto-progetti-python-machine-learning-deep-learning.html"
+          target="_blank"
+        >
+          <h2>SuperProf</h2>
+        </a>
+        <a
+          href="https://www.youtube.com/@salamander_xing"
+          target="_blank"
+        >
+          <h2>YouTube</h2>
+        </a>
+      </div>
+    ),
+    Projects: () => (
+      <div>
+        <h1>IN EVIDENCE</h1>
+        <div>
+          <a href="https://github.com/SalamanderXing/mate" target="_blank">
+            <h2>Maté</h2>
+          </a>
+          <a href="https://github.com/salamanderxing" target="_blank">
+            <h2>Full list on GitHub</h2>
+          </a>
+        </div>
+        <h1>ONLINE</h1>
+        <div>
+          <a
+            href="https://salamanderxing.github.io/hopfield-network/"
+            target="_blank"
+          >
+            <h2>Stochastic Hopfield Network</h2>
+          </a>
+          <a href="https://salamanderxing.github.io/cannon/" target="_blank">
+            <h2>Cannon</h2>
+          </a>
+          <a href="https://github.com/salamanderxing/mcs" target="_blank">
+            <h2>MCS</h2>
+          </a>
+          <a href="https://salamanderxing.github.io/focus/" target="_blank">
+            <h2>Focus</h2>
+          </a>
+        </div>
+      </div>
+    ),
     Blog: () => <div>Blog</div>,
   };
 
