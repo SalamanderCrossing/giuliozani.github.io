13c1277fdc122c8081d5a3fb7bc8b14fc48c49d3
salamandercrossing <giulio.zani@gmail.com>
2023-01-10 21:09:56 +0000

    added tutorial

--- main/src/About.tsx
+++ main/src/About.tsx
@@ -19,7 +19,7 @@ export default () => {
       <p>
         Hey there! I'm a master's student in AI at Maastricht University and I'm
         currently doing my research internship at the University of Amsterdam.
-        I'm super passionate about deep learning, computer science, and cryptos.
+        I'm super passionate about deep learning, computer science, and blockchain technologies.
       </p>
       <p>
       I give private lessons in the field of computer programming, artificial intelligence (deep learning) and machine learning. I'm also available as a consultant for your project or thesis. Please check out the Contact section if you are interested.
--- main/src/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import NavBar from "./NavBar";
-
-function Home() {
-  return <div>Hey</div>;
-}
-
-export default Home;
